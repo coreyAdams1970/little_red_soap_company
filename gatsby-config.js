c9a54a152e52f31832700b61942d51ceb405b79a
corey smaller <coreysmaller@gmail.com>
2021-04-08 15:40:08 -0600

    first

---
+++ gatsby-config.js
@@ -0,0 +1,143 @@
+module.exports = {
+  siteMetadata: {
+    // edit below
+    title: `Kick It Up A Notch Alpine and Desert Design`,
+    author: `Corey Smaller`,
+    description: `landscape design in AZ and UT`,
+    siteUrl: `https://www.kickitupdesertdesign.com`,
+    social: {
+      twitter: `gatsbyjs`,
+      instagram: `kickitupanotch`
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-feed-mdx`,
+    {
+      resolve: "gatsby-plugin-local-search",
+      options: {
+        name: "blog",
+        engine: "flexsearch",
+        engineOptions: {
+          encode: "icase",
+          tokenize: "forward",
+          async: false,
+        },
+        query: `
+          {
+            allMdx {
+              nodes {
+                id
+                fields { slug }
+                excerpt
+                rawBody
+                frontmatter {
+                  title
+                  description
+                  date(formatString: "MMMM DD, YYYY")
+                }
+              }
+            }
+          }
+        `,
+        ref: "id",
+        index: ["title", "rawBody"],
+        store: ["id", "slug", "date", "title", "excerpt", "description"],
+        normalizer: ({ data }) =>
+          data.allMdx.nodes.map(node => ({
+            id: node.id,
+            slug: node.fields.slug,
+            rawBody: node.rawBody,
+            excerpt: node.excerpt,
+            title: node.frontmatter.title,
+            description: node.frontmatter.description,
+            date: node.frontmatter.date,
+          })),
+      },
+    },
+    `gatsby-plugin-feed-mdx`,
+    `gatsby-plugin-root-import`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [".mdx", ".md"],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-vscode`,
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+          },
+          {
+            resolve: `gatsby-remark-smartypants`,
+          },
+        ],
+        plugins: [`gatsby-remark-images`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // edit below
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        // edit below
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
